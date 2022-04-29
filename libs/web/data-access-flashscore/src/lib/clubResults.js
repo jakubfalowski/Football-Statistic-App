1deaa47f3e7a9a1afcbe4c23e51725c5b0db4e7b
jakubfalowski <kubafa99@gmail.com>
2022-04-29 14:41:10 +0200

    fix: fetch on button

--- libs/web/data-access-flashscore/src/lib/clubResults.js
+++ libs/web/data-access-flashscore/src/lib/clubResults.js
@@ -9,28 +9,26 @@ const options = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'flashscore.p.rapidapi.com',
-    'X-RapidAPI-Key': '35ffe6e4c5mshe6f63287717ea95p1950a0jsna16c5a14a971'
+    'X-RapidAPI-Key': '1941a7725amshe72888a2f321827p18582bjsnddf354460ab2'
   }
 };
 
-// 1941a7725amshe72888a2f321827p18582bjsnddf354460ab2 KEY2
+// 35ffe6e4c5mshe6f63287717ea95p1950a0jsna16c5a14a971 KEY2
 
 export function ClubResults(){
     const [clubs, setClubs] = useState([]);
 
       if (!query) return<></>;
   
-    //   async function fetchData() {
-    //     const response = await fetch(
-    //       `https://flashscore.p.rapidapi.com/v1/teams/results?sport_id=1&team_id=${query}&locale=en_GB&page=1`,options
-    //     );
-    //     const data = await response.json();
-    //     const results = data.DATA.EVENTS;
-    //     console.log(results);
-    //     setClubs(results);
-    //   }
-      fetchData();
-
+      async function fetchData() {
+        const response = await fetch(
+          `https://flashscore.p.rapidapi.com/v1/teams/results?sport_id=1&team_id=${query}&locale=en_GB&page=1`,options
+        );
+        const data = await response.json();
+        const results = data.DATA[0].EVENTS;
+        console.log(results);
+        setClubs(results);
+      }
 
     return(
         <div>
@@ -41,7 +39,7 @@ export function ClubResults(){
                 <th>Gospodarze</th>
                 <th>Goście</th>
               </tr>
-            {/* {clubs !== undefined ? clubs.map((item, i) => {
+            {clubs !== undefined ? clubs.map((item, i) => {
             return (
               <tr key={i}>
                 <td>{item.HOME_NAME} {item.HOME_SCORE_CURRENT}</td>
@@ -51,15 +49,12 @@ export function ClubResults(){
             }): 
             <tr>
                 <td colspan="2">Brak wyników wyszukiwania</td>
-            </tr>} */}
+            </tr>}
             </table>
+            <button onClick={fetchData}>XD</button>
         </div>
     )
 }
-
-// FlashscoreClub.propTypes = {
-//     query: PropTypes.string
-// }
 
 export default ClubResults;
 
