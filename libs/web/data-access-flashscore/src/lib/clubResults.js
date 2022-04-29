9f7cea9564b808661a0e030872274ef855bedf25
jakubfalowski <kubafa99@gmail.com>
2022-04-29 21:47:40 +0200

    feat: home away strenght

--- libs/web/data-access-flashscore/src/lib/clubResults.js
+++ libs/web/data-access-flashscore/src/lib/clubResults.js
@@ -11,38 +11,65 @@ const options = {
 
 // 35ffe6e4c5mshe6f63287717ea95p1950a0jsna16c5a14a971 KEY2
 
-export function ClubResults(){
+export function ClubResults(query2){
   const {query} = useParams();
     const [clubs, setClubs] = useState([]);
     let teamValue = 0;
+    let homeValue = 0; 
+    let awayValue = 0;
+    let homeMatches = [];
+    let awayMatches = [];
     let multiplier = 1;
-
-      if (!query) return<></>;
   
       async function fetchData() {
         const response = await fetch(
-          `https://flashscore.p.rapidapi.com/v1/teams/results?sport_id=1&team_id=${query}&locale=en_GB&page=1`,options
+          `https://flashscore.p.rapidapi.com/v1/teams/results?sport_id=1&team_id=${query2}&locale=en_GB&page=1`,options
         );
         const data = await response.json();
         const results = data.DATA[0].EVENTS;
         console.log(results);
         setClubs(results);
       }
-      if(clubs !== undefined){
+
+      function teamStrength(){
+
         clubs.slice(clubs.length-15, clubs.length).map((item, lastMatches) => {
           if(lastMatches < 5) multiplier = 0.75
           else if(lastMatches < 10) multiplier = 1
           else multiplier = 1.25
 
           if(item.HOME_SCORE_CURRENT === item.AWAY_SCORE_CURRENT) teamValue += (1*multiplier);
-          if(item.HOME_PARTICIPANT_IDS[0]===query){
+          if(item.HOME_PARTICIPANT_IDS[0]===query2){
             if(item.HOME_SCORE_CURRENT > item.AWAY_SCORE_CURRENT) teamValue += (3*multiplier);
+            homeMatches.push(item)
           }
           else{
             if(item.HOME_SCORE_CURRENT < item.AWAY_SCORE_CURRENT) teamValue += (3*multiplier);
+            awayMatches.push(item)
           }
         })
-      }
+      
+        homeMatches.slice(homeMatches.length-5,homeMatches.length).map(item =>{
+          if(item.HOME_SCORE_CURRENT === item.AWAY_SCORE_CURRENT) homeValue += 1;
+          if(item.HOME_PARTICIPANT_IDS[0]===query2){
+            if(item.HOME_SCORE_CURRENT > item.AWAY_SCORE_CURRENT) homeValue += 3;
+          }
+          else{
+            if(item.HOME_SCORE_CURRENT < item.AWAY_SCORE_CURRENT) homeValue += 3;
+          }
+        })
+
+        awayMatches.slice(awayMatches.length-5,awayMatches.length).map(item =>{
+          if(item.HOME_SCORE_CURRENT === item.AWAY_SCORE_CURRENT) awayValue += 1;
+          if(item.AWAY_PARTICIPANT_IDS[0]==query2){
+            if(item.HOME_SCORE_CURRENT < item.AWAY_SCORE_CURRENT) awayValue += 3;
+          }
+          else{
+            if(item.HOME_SCORE_CURRENT > item.AWAY_SCORE_CURRENT) awayValue += 3;
+          }
+        })
+    }
+    if(clubs !== undefined) teamStrength();
 
     return(
         <div>
@@ -66,7 +93,7 @@ export function ClubResults(){
                 <td colspan="2">Brak wyników wyszukiwania</td>
             </tr>}
             </table>
-            <b>{teamValue}</b>
+            <b>Forma zespołu: {teamValue} <br/>U siebie: {(homeValue)}pkt.<br/>Na wyjeździe: {awayValue}pkt.<br/></b>
             <button onClick={fetchData}>XD</button>
         </div>
     )
---
+++ libs/web/data-access-flashscore/src/lib/pageIndex.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ClubResults from "./clubResults";
+
+export class pageIndex extends React.Component{
+    render(){
+        return(
+            <ClubResults/>
+        )
+    }
+    
+}
